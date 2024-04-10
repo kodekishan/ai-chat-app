5e00515135959162923b509eba0a571045f4765c
Kishan Kesari Gupta <kishankesri1996@gmail.com>
2024-04-10 07:53:42 +0530

    created the dashboard that will list out the AI user for chatting

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,12 +1,23 @@
 "use client";
 
+import { cn } from "@/lib/utils";
 import { UserButton } from "@clerk/nextjs";
+import { Poppins } from "next/font/google";
+
+const font = Poppins({ weight: "600", subsets: ["latin"] });
 
 export const Navbar = () => {
   return (
-    <div className="fixed w-full z-50 flex justify-between items-center py-2 px-4 border-b border-primary/10 bg-sky-100">
+    <div className="fixed w-full z-50 flex justify-between items-center py-2 px-4 border-b border-primary/20 bg-sky-600 h-16">
       <div className="flex items-center">
-        <h1>AI Chatting interface</h1>
+        <h1
+          className={cn(
+            "text-xl md:text-3xl font-bold text-white",
+            font.className
+          )}
+        >
+          AI Chatting interface
+        </h1>
       </div>
       <div className="flex items-center gap-x-3">
         <UserButton />
---
+++ components/sidebar.tsx
@@ -0,0 +1,59 @@
+"use client";
+
+import { cn } from "@/lib/utils";
+import { Home, Plus, Settings } from "lucide-react";
+import { usePathname, useRouter } from "next/navigation";
+
+export const Sidebar = () => {
+  const pathname = usePathname();
+  const router = useRouter();
+
+  const routes = [
+    {
+      icon: Home,
+      href: "/",
+      label: "Home",
+      pro: false,
+    },
+    {
+      icon: Plus,
+      href: "/user/new",
+      label: "Create",
+      pro: false,
+    },
+    {
+      icon: Settings,
+      href: "/settings",
+      label: "Settings",
+      pro: false,
+    },
+  ];
+
+  const onNavigate = (url: string, pro: boolean) => {
+    return router.push(url);
+  };
+
+  return (
+    <div className="space-y-4 flex flex-col h-full border-r border-primary/20 text-primary bg-white">
+      <div className="flex flex-1 justify-center">
+        <div className="space-y-2">
+          {routes.map((route) => (
+            <div
+              key={route.href}
+              onClick={() => onNavigate(route.href, route.pro)}
+              className={cn(
+                "text-muted-foreground text-black text-xs group flex p-3 w-full justify-start font-medium cursor-pointer hover:text-sky-600 hover:bg-primary/10 rounded-lg transition",
+                pathname === route.href && "bg-primary/10 text-sky-600"
+              )}
+            >
+              <div className="flex flex-col gap-y-2 items-center flex-1">
+                <route.icon className="w-5 h-5" />
+                {route.label}
+              </div>
+            </div>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
