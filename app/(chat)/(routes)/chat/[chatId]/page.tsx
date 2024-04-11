3b256dde59551ecf22163629cbe0aab7be5fccfe
Kishan Kesari Gupta <kishankesri1996@gmail.com>
2024-04-11 21:46:09 +0530

    Added the chat page with specific id

---
+++ app/(chat)/(routes)/chat/[chatId]/page.tsx
@@ -0,0 +1,39 @@
+import prismadb from "@/lib/prismadb";
+import { auth, redirectToSignIn } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+import { ChatClient } from "./components/chat-client";
+
+interface ChatIdPageProps {
+  params: {
+    chatId: string;
+  };
+}
+
+const ChatIdPage = async ({ params }: ChatIdPageProps) => {
+  const { userId } = auth();
+  if (!userId) return redirectToSignIn();
+  const robot = await prismadb.robot.findUnique({
+    where: { id: params.chatId },
+    include: {
+      messages: {
+        orderBy: {
+          createdAt: "asc",
+        },
+        where: {
+          userId,
+        },
+      },
+      _count: {
+        select: {
+          messages: true,
+        },
+      },
+    },
+  });
+
+  if (!robot) return redirect("/");
+
+  return <ChatClient robot={robot} />;
+};
+
+export default ChatIdPage;
