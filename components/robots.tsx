213cebf4a9b83db44daa45a063bc2d2ef9e118a4
Kishan Kesari Gupta <kishankesri1996@gmail.com>
2024-04-11 18:32:32 +0530

    Updated the components to list down the chat robots that need to responds are messages

--- components/robots.tsx
+++ components/robots.tsx
@@ -1,21 +1,53 @@
-// "use client";
+import { Robot } from "@prisma/client";
+import Image from "next/image";
+import { Card, CardFooter, CardHeader } from "@/components/ui/card";
+import Link from "next/link";
+import { MessagesSquare } from "lucide-react";
 
-// import { Robot } from "@prisma/client";
-// import { Button } from "./ui/button";
-// import { cn } from "@/lib/utils";
+interface RobotsProp {
+  data: (Robot & { _count: { messages: number } })[];
+}
 
-// interface RobotsProps {
-//   data: Robot[];
-// }
-
-// export const Robots = ({ data }: RobotsProps) => {
-//   return (
-//     <div className="w-full overflow-x-auto space-x-2 flex p-1">
-//       <Button
-//         className={cn(
-//           `flex items-center text-center text-xs md:text-sm px-2 md:px-4 py-2 md:py-3 rounded-md bg-primary/10 hover:opacity-75 transition`
-//         )}
-//       ></Button>
-//     </div>
-//   );
-// };
+export const Robots = ({ data }: RobotsProp) => {
+  if (data?.length === 0) {
+    return (
+      <div className="pt-10 flex flex-col items-center justify-center space-y-3">
+        <div className="relative w-80 h-80">
+          <Image fill alt="empty" src="/empty.png" />
+        </div>
+      </div>
+    );
+  }
+  return (
+    <div className="mx-12 grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-col-6 gap-2 pb-10">
+      {data.map((item) => (
+        <Card
+          key={item.id}
+          className="bg-white rounded-xl cursor-pointer hover:opacity-75 transition border-0"
+        >
+          <Link href={`/chat/${item.id}`}>
+            <CardHeader className="flex items-center justify-center text-center text-muted-foreground">
+              <div className="relative w-32 h-32">
+                <Image
+                  fill
+                  src={item.src}
+                  className="rounded-xl object-cover"
+                  alt={`robot-${item.id}`}
+                />
+              </div>
+              <p className="text-lg font-bold text-sky-600">{item.name}</p>
+              <p className="text-sm text-black">{item.description}</p>
+            </CardHeader>
+            <CardFooter className="flex items-center justify-between text-xs text-muted-foreground">
+              <p className="lowercase">@{item.userName}</p>
+              <div className="flex item-center text-sky-500">
+                <MessagesSquare className="w-4 h-4 mr-1" />
+                {item._count.messages}
+              </div>
+            </CardFooter>
+          </Link>
+        </Card>
+      ))}
+    </div>
+  );
+};
