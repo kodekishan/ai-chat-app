752e169b56ed7decb55c79d23da2c4277af69a95
Kishan Kesari Gupta <kishankesri1996@gmail.com>
2024-04-11 09:34:20 +0530

    Added the form for creating the robots

--- app/(root)/(routes)/robot/[robotId]/components/robot-form.tsx
+++ app/(root)/(routes)/robot/[robotId]/components/robot-form.tsx
@@ -9,8 +9,17 @@ import {
   FormField,
   FormItem,
   FormMessage,
+  FormLabel,
+  FormDescription,
 } from "@/components/ui/form";
 import { Separator } from "@/components/ui/separator";
+import { ImageUpload } from "@/components/image-upload";
+import { Input } from "@/components/ui/input";
+import { Textarea } from "@/components/ui/textarea";
+import { PREAMBLE } from "@/robots/robo-text";
+import { SEED_CHAT } from "@/robots/robo-text";
+import { Button } from "@/components/ui/button";
+import { Wand2 } from "lucide-react";
 
 interface RobotFormProps {
   data: Robot | null;
@@ -53,7 +62,9 @@ export const RobotForm = ({ data }: RobotFormProps) => {
           <div className="space-y-2 w-full">
             <div>
               <h3 className="text-lg font-medium">General Information</h3>
-              <p>General information about AI robot.</p>
+              <p className="text-sm text-muted-foreground">
+                Basic details on artificial intelligence robots
+              </p>
             </div>
             <Separator className="bg-primary/10" />
           </div>
@@ -61,11 +72,120 @@ export const RobotForm = ({ data }: RobotFormProps) => {
             name="src"
             render={({ field }) => (
               <FormItem className="flex flex-col items-center justify-center space-y-4">
-                <FormControl>Image Upload Component</FormControl>
+                <FormControl>
+                  <ImageUpload
+                    disabled={isLoading}
+                    onChange={field.onChange}
+                    value={field.value}
+                  />
+                </FormControl>
                 <FormMessage />
               </FormItem>
             )}
           />
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+            <FormField
+              name="name"
+              control={form.control}
+              render={({ field }) => (
+                <FormItem className="col-span-2 md:col-span-1">
+                  <FormLabel>Name</FormLabel>
+                  <FormControl>
+                    <Input
+                      disabled={isLoading}
+                      placeholder="Smith Doe"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormDescription>
+                    This is the name that your AI robot will have.
+                  </FormDescription>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              name="description"
+              control={form.control}
+              render={({ field }) => (
+                <FormItem className="col-span-2 md:col-span-1">
+                  <FormLabel>Description</FormLabel>
+                  <FormControl>
+                    <Input
+                      disabled={isLoading}
+                      placeholder="Founder of Boring Company"
+                      {...field}
+                    />
+                  </FormControl>
+                  <FormDescription>
+                    An overview of your AI robot in brief.
+                  </FormDescription>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </div>
+          <div className="space-y-2 w-full">
+            <div>
+              <h3 className="text-lg font-medium"> Configuration</h3>
+              <p className="text-sm text-muted-foreground">
+                Comprehensive guidelines for AI conduct
+              </p>
+            </div>
+            <Separator className="bg-primary/10" />
+          </div>
+          <FormField
+            name="instruction"
+            control={form.control}
+            render={({ field }) => (
+              <FormItem className="col-span-2 md:col-span-1">
+                <FormLabel>Instructions</FormLabel>
+                <FormControl>
+                  <Textarea
+                    className="bg-background resize-none"
+                    rows={7}
+                    disabled={isLoading}
+                    placeholder={PREAMBLE}
+                    {...field}
+                  />
+                </FormControl>
+                <FormDescription>
+                  Give a thorough account of your robot&apos;s history and any
+                  pertinent information.
+                </FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            name="seed"
+            control={form.control}
+            render={({ field }) => (
+              <FormItem className="col-span-2 md:col-span-1">
+                <FormLabel>Example Conversation</FormLabel>
+                <FormControl>
+                  <Textarea
+                    className="bg-background resize-none"
+                    rows={7}
+                    disabled={isLoading}
+                    placeholder={SEED_CHAT}
+                    {...field}
+                  />
+                </FormControl>
+                <FormDescription>
+                  An example dialogue regarding how to receive an AI robot
+                  response is shown above.
+                </FormDescription>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <div className="w-full flex justify-center">
+            <Button variant="skyBlue" size="lg" disabled={isLoading}>
+              {data ? "Edit your robot" : "Create your robot"}
+              <Wand2 className="w-4 h-4 ml-2" />
+            </Button>
+          </div>
         </form>
       </Form>
     </div>
