5f6ba8517d6d88e6f971ae16e0e6b72eea0dcdb7
Kishan Kesari Gupta <kishankesri1996@gmail.com>
2024-04-11 18:34:05 +0530

    Updated the component to get the chat robots on search

--- app/(root)/(routes)/page.tsx
+++ app/(root)/(routes)/page.tsx
@@ -1,11 +1,33 @@
-// import { Robots } from "@/components/robots";
 import { SearchInput } from "@/components/search-input";
+import prismadb from "@/lib/prismadb";
+import { Robots } from "@/components/robots";
 
-const RootPage = async () => {
+interface RootPageProps {
+  searchParams: {
+    name: string;
+  };
+}
+
+const RootPage = async ({ searchParams }: RootPageProps) => {
+  const robotData = await prismadb.robot.findMany({
+    where: {
+      name: searchParams.name,
+    },
+    orderBy: {
+      createdAt: "desc",
+    },
+    include: {
+      _count: {
+        select: {
+          messages: true,
+        },
+      },
+    },
+  });
   return (
-    <div className="h-full p-4 space-y-2">
-      {/* <SearchInput /> */}
-      {/* <Robots data={aiRobots} /> */}
+    <div className="h-full p-4 space-y-2 bg-sky-50">
+      <SearchInput />
+      <Robots data={robotData} />
     </div>
   );
 };
