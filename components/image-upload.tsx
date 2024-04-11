89624252f602673bb017aa056a13cac93e8a4538
Kishan Kesari Gupta <kishankesri1996@gmail.com>
2024-04-11 09:33:36 +0530

    Added the functionality for image uploading

--- components/image-upload.tsx
+++ components/image-upload.tsx
@@ -2,7 +2,7 @@
 
 import { useEffect, useState } from "react";
 import { CldUploadButton } from "next-cloudinary";
-
+import Image from "next/image";
 
 interface ImageUploadProps {
   value: string;
@@ -27,10 +27,22 @@ export const ImageUpload = ({
 
   return (
     <div className="space-y-4 w-full flex flex-col justify-center items-center">
-      <CldUploadButton>
-        
+      <CldUploadButton
+        onUpload={(result: any) => onChange(result.info.secure_url)}
+        options={{ maxFiles: 1 }}
+        uploadPreset="kzuzn4zi"
+      >
+        <div className="p-4 border-4 border-dashed border-primary/10 rounded-lg hover:opacity-75 transition flex flex-col space-y-2 items-center justify-center">
+          <div className="relative h-40 w-40">
+            <Image
+              fill
+              alt="Upload"
+              src={value || "/placeholder.png"}
+              className="rounded-lg object-cover"
+            />
+          </div>
+        </div>
       </CldUploadButton>
     </div>
-  )
-
+  );
 };
