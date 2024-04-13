0e53a27adb0f3018c89b4df6e19f60b15287eb81
Kishan Kesari Gupta <kishankesri1996@gmail.com>
2024-04-13 16:03:22 +0530

    Created the chat messages and message component

--- components/chat-messages.tsx
+++ components/chat-messages.tsx
@@ -1,10 +1,12 @@
 "use client";
 
 import { Robot } from "@prisma/client";
-import { ChatMessage } from "@/components/chat-message";
+import { ChatMessage, ChatMessageProps } from "@/components/chat-message";
+import { ElementRef, useEffect, useRef, useState } from "react";
+import { useRouter } from "next/navigation";
 
 interface ChatMessagesProps {
-  messages: any[];
+  messages: ChatMessageProps[];
   isLoading: boolean;
   robot: Robot;
 }
@@ -14,9 +16,42 @@ export const ChatMessages = ({
   isLoading,
   robot,
 }: ChatMessagesProps) => {
+  const router = useRouter();
+  const scrollRef = useRef<ElementRef<"div">>(null);
+
+  const [loading, setLoading] = useState(messages.length === 0 ? true : false);
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setLoading(false);
+    }, 2000);
+    return () => {
+      clearTimeout(timeout);
+    };
+  }, []);
+
+  useEffect(() => {
+    scrollRef?.current?.scrollIntoView({ behavior: "smooth" });
+  }, [messages.length]);
+
   return (
     <div className="flex-1 overflow-y-auto pr-4">
-      <ChatMessage />
+      <ChatMessage
+        isLoading={loading}
+        src={robot.src}
+        role="robot"
+        content={`Hello, I am ${robot.name}, ${robot.description}`}
+      />
+      {messages.map((message) => (
+        <ChatMessage
+          key={message.content}
+          role={message.role}
+          content={message.content}
+          src={robot.src}
+        />
+      ))}
+      {isLoading && <ChatMessage role="robot" src={robot.src} isLoading />}
+      <div ref={scrollRef} />
     </div>
   );
 };
